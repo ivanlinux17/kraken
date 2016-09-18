897cb841a386ab1bd23d6ea3d150af30a2a6239a
Ivan <ivanlinux@gmail.com>
2016-09-17 23:13:42 -0600

    Added logging capabilities ;-)

--- Kraken/kraken.cpp
+++ Kraken/kraken.cpp
@@ -4,6 +4,7 @@
 #include <QDesktopServices>
 #include <QFileInfo>
 #include <QtCore>
+#include <QMessageBox>
 
 
 Kraken::Kraken(QWidget *parent) :
@@ -28,6 +29,7 @@ void Kraken::Initialize()
 {
     // Read Ini
     settings = new QSettings("../kraken.ini", QSettings::IniFormat);
+    timerLogging = new QTimer(this);
     // Initial values
     ui->ageSpin->setRange(settings->value("minAge",0).toInt(),
                           settings->value("maxAge",130).toInt());
@@ -35,6 +37,8 @@ void Kraken::Initialize()
                             settings->value("maxAge",130).toInt());
     QPixmap image("../pics/miniKraken.jpg");
     ui->pictureLabel->setPixmap(image);
+    connect(timerLogging, SIGNAL(timeout()), this, SLOT(on_timer()));
+
 
     // Connecting new way
     QObject::connect (ui->ageSpin, static_cast<void (QSpinBox::*)(int)> (&QSpinBox::valueChanged),
@@ -45,6 +49,8 @@ void Kraken::Initialize()
 
 
     ui->ageSpin->setValue(settings->value("lastAge",35).toInt());
+    timerLogging->start(100);
+    ui->logEdit->setMaximumBlockCount(80);
 }
 
 /**
@@ -66,4 +72,78 @@ void Kraken::on_actionManual_triggered()
 {
     //QDesktopServices::openUrl(QUrl("file:///C:/Documents and Settings/All Users/Desktop", QUrl::TolerantMode));
     QDesktopServices::openUrl(QUrl::fromLocalFile(QFileInfo("../docs/html/index.html").absoluteFilePath()));
+}
+
+void Kraken::on_pushButton_clicked()
+{
+    Qt::CaseSensitivity cs = Qt::CaseInsensitive;
+    QString searchKey = ui->searchKeyEdit->text();
+}
+
+void Kraken::on_timer()
+{
+    QString text;
+    int value = qrand() % 10;
+    if (value <= 2)
+    {
+        text = QString("<font color=\"red\">18/12 Error Oh my god! An error ocurred</font>");
+        if (ui->errorsCheck->isChecked())
+            ui->logEdit->appendHtml(text);
+    }
+    else
+    {
+        text = QString ("18/12 Trace The value returned from the function was %1")
+                .arg(value);
+        if (ui->traceCheck->isChecked())
+            ui->logEdit->appendHtml(text);
+    }
+    this->lines.append(text);
+    if (lines.count()>80)
+        lines.pop_front();
+
+    QString mess = QString ("lines size %1 logedit size %2.")
+                           .arg(lines.count()).arg(ui->logEdit->blockCount());
+    std::cout<<mess.toStdString()<<std::endl;
+}
+
+void Kraken::on_clearButton_clicked()
+{
+    ui->logEdit->clear();
+}
+void Kraken::filterLog()
+{
+    ui->logEdit->clear();
+
+    for (int i = 0; i < lines.size();i++)
+    {
+        QString line = lines[i];
+        if (line.contains("Error") && ui->errorsCheck->isChecked())
+            ui->logEdit->appendHtml(line);
+        else
+        {
+            if (line.contains("Trace") && ui->traceCheck->isChecked())
+                ui->logEdit->appendHtml(line);
+        }
+    }
+}
+
+void Kraken::on_traceCheck_stateChanged(int)
+{
+    filterLog();
+}
+
+void Kraken::on_stopButton_clicked()
+{
+    this->timerLogging->stop();
+}
+
+
+void Kraken::on_errorsCheck_stateChanged(int)
+{
+    filterLog();
+}
+
+void Kraken::on_pushButton_2_clicked()
+{
+    timerLogging->start();
 }
--- Kraken/kraken.h
+++ Kraken/kraken.h
@@ -14,6 +14,13 @@
 #include <QMainWindow>
 #include <QSettings>
 #include <iostream>
+#include <vector>
+
+struct logType
+{
+    int type;
+    QString message;
+};
 
 namespace Ui {
 class Kraken;
@@ -38,14 +45,35 @@ private slots:
 
     void on_actionManual_triggered();
 
+    void on_pushButton_clicked();
+
+    void on_timer();
+
+    void on_clearButton_clicked();
+
+    void on_traceCheck_stateChanged(int arg1);
+
+    void on_stopButton_clicked();
+
+    void on_errorsCheck_stateChanged(int arg1);
+
+    void on_pushButton_2_clicked();
+
 public slots:
     void Closing();
 
 private:
     Ui::Kraken *ui;
+    std::vector<logType> log;
+    QString loggingtext;
+    QStringList lines;
     void Initialize();
+    void filterLog ();
 
     QSettings *settings;
+
+
+    QTimer *timerLogging;
 };
 
 #endif // KRAKEN_H
